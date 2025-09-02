b79f5130234abfec01615bd1e66a34fece5ff2bd
Ahmed Abbas <iahmedabbas7@gmail.com>
2025-09-03 00:51:36 +0500

    Added Lesson 1 for the TypeScript Journey

---
+++ Lesson-1/build/js/main.js
@@ -0,0 +1,13 @@
+"use strict";
+let hobbies;
+let myAge;
+let myName;
+let amIMuslim;
+let album;
+album = "12";
+(myAge = 25), (amIMuslim = true);
+hobbies = "Football";
+myName = "Ahmed Abbas";
+const sum = (a, b) => {
+    return a + b;
+};
---
+++ Lesson-1/src/main.ts
@@ -0,0 +1,14 @@
+let hobbies: any;
+let myAge: number;
+let myName: string;
+let amIMuslim: boolean;
+let album: any;
+
+album = "12";
+(myAge = 25), (amIMuslim = true);
+hobbies = "Football";
+myName = "Ahmed Abbas";
+
+const sum = (a: number, b: number) => {
+  return a + b;
+};
